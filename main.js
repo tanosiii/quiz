86057b2f09d9d17ef9f58feb5b6cc140d3e449b9
tanosiii <91964264+tanosiii@users.noreply.github.com>
2025-03-15 10:03:32 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,115 @@
+const img  = document.getElementById("image");
+const option1 = document.getElementById("option-1");
+const option2 = document.getElementById("option-2");
+const qnum = document.getElementById("q-num");
+const tbl = document.getElementById("table");
+const resultBtn = document.getElementById("resultBtn");
+const finishText = document.getElementById("finishText");
+const btnAudio = document.getElementById("btn-audio");
+
+//今何問目？
+let quizNumber = 1;
+
+//問題の画像
+const images = ["./hamabe1.jpg","./gakki3.jpg","./gakki1.jpeg"
+    ,"./gakki2.jpeg","./aoi1.webp","./gakki4.jpeg"
+    ,"./ayase1.jpeg","./nagasawa1.jpg","./gakki5.jpeg"
+    ,"./gakki6.jpg"
+]
+
+//答え
+const answer = ["no","yes","yes"
+    ,"yes","no","yes",
+    "no","no","yes",
+    "yes"
+]
+
+//正誤の記録
+let record = [];
+
+//押された時
+option1.addEventListener('click',()=>{
+    
+    //記録
+    record[quizNumber-1] = answer[quizNumber-1] == "yes" ? true : false;
+
+    //効果音
+    btnAudio.play();
+
+    //最後じゃなかったら次
+    if(quizNumber < 10)
+        nextQuestion();
+
+    //最後なら結果につながるボタンを表示する
+    else if(quizNumber==10)
+        prepareForResult();
+  });
+
+option2.onclick = function(){
+
+    //記録
+    record[quizNumber-1] = answer[quizNumber-1] == "no" ? true : false;
+
+    //効果音
+    btnAudio.play();
+
+     //最後じゃなかったら次
+    if(quizNumber < 10)
+         nextQuestion();
+
+    //最後なら結果につながるボタンを表示する
+     else if(quizNumber==10)
+         prepareForResult();
+};
+
+//”結果を見る”ボタンの表示
+resultBtn.onclick = function(){
+    tbl.style.visibility = "visible";
+};
+
+//次の問題へ
+function nextQuestion(){
+    //n+1問目
+    quizNumber++;
+
+    //n問目表示
+    qnum.textContent = quizNumber + "/10問";
+
+    //画像変更
+    img.src = images[quizNumber-1];
+
+}
+
+//結果を表示する準備をする
+function prepareForResult(){
+    resultBtn.style.display = "block";
+    img.style.display = "none";
+    option1.style.display = "none";
+    option2.style.display = "none";
+    finishText.style.visibility = "visible";
+    //qnum.style.display = "none";
+
+     // テーブルの作成
+     for (var i = 0; i < 10; i++) {
+        // tr要素を生成
+        var tr = document.createElement('tr');
+
+        // th・td部分のループ
+        for (var j = 0; j < 2; j++) {
+        
+              // td要素を生成
+              var td = document.createElement('td');
+              // td要素内にテキストを追加
+              if(j==0)
+                td.textContent = i+1;
+              else if(j==1)
+                    td.textContent = record[i]==true　?　"⭕️": "✖️";
+                
+              // td要素をtr要素の子要素に追加
+              tr.appendChild(td);
+            
+        }
+        // tr要素をtable要素の子要素に追加
+        tbl.appendChild(tr);
+    }
+}
